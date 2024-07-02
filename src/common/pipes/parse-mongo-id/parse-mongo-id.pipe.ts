4b187f040e36b5f98d6ec1dc0b5a0dc6e299f15c
srueda <srueda@ec.krugercorp.com>
2024-07-02 08:41:31 -0500

    antes de insertar por lote

--- src/common/pipes/parse-mongo-id/parse-mongo-id.pipe.ts
+++ src/common/pipes/parse-mongo-id/parse-mongo-id.pipe.ts
@@ -9,7 +9,6 @@ export class ParseMongoIdPipe implements PipeTransform {
     console.log({value, metadata});
     if(!isValidObjectId(value)){
       throw new BadRequestException(`${value} is not a valid mongo id`);
-      
     }
     return value;
   }
